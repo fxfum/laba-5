a48d45655a062167bc532688d47f9c0125019c0a
Студент Студентов <is@athene.tech>
2025-12-12 09:31:41 +0300

    comit1

---
+++ Project1/FileName.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <Windows.h> 
+#include <math.h>
+
+void main()
+{
+	int a, b, min, max;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+	//SetConsoleCP(1251);
+	//SetConsoleOutputCP(1251);
+
+	printf("Введите первое число = ");
+	scanf_s("%d", &a);
+	printf("Введите второе число = ");
+	scanf_s("%d", &b);
+	if (a < b) {
+		min = a;
+		max = b;
+	}
+	else {
+		min = b;
+		max = a;
+	}
+	printf("Большее число  = %d\n", max);
+	printf("Меньше число = %d\n", min);
+
+}
